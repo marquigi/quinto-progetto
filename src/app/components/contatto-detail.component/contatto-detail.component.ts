3a4cdbbab17d92f51a578e95d1bb4731f1b56d99
marquigi <markluigi3@yahoo.it>
2025-10-28 23:02:55 +0100

    .

---
+++ src/app/components/contatto-detail.component/contatto-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contatto-detail.component',
+  imports: [],
+  templateUrl: './contatto-detail.component.html',
+  styleUrl: './contatto-detail.component.css',
+})
+export class ContattoDetailComponent {
+
+}
